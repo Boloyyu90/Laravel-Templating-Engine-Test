23937626400a819eba9538e0411dd5bf1624d33e
Bala Putra <balaputra20@gmail.com>
2023-10-07 22:31:19 +0700

    npm_kurang

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -112,7 +112,7 @@
     <footer class="main-footer">
       <!-- To the right -->
       <div class="float-right d-none d-sm-inline">
-        210711320
+        2107113
       </div>
       <!-- Default to the left -->
       <strong>Copyright &copy; {{ date('Y') }} <a href="#">Gofit</a>. </strong> All rights reserved.
